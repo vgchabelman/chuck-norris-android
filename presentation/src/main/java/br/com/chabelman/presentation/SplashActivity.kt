24ef7da914bb3bc32af64307f9b0b41373ffbd9c
Victor Chabelman <vgchabelman@gmail.com>
2021-05-14 10:09:20 -0300

    Fix Splash onBackPressed

--- presentation/src/main/java/br/com/chabelman/presentation/SplashActivity.kt
+++ presentation/src/main/java/br/com/chabelman/presentation/SplashActivity.kt
@@ -14,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
             if (isFinishing) return
             val intent = Intent(baseContext, BaseActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 
